2546f27a7d1b8aa4bbdca852124b7ec5d80d3bc9
pserrano <pserrano@uolinc.com>
2020-03-03 14:26:55 -0300

    Declaring Variables in Kotlin

---
+++ src/com/pgserrano/kotlincourse/basic/Variables.kt
@@ -0,0 +1,11 @@
+package com.pgserrano.kotlincourse.basic
+
+fun main(args: Array<String>) {
+
+    var streetNumber: Int
+    var streetName: String = "High Street"
+    var streetNameInference = "Low Street"
+
+
+    val zip = "E11 P1" //Immutable Variable
+}
